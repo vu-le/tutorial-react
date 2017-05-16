218096003a4a9e4451ad41d1f989f00144eda075
vu-le <vu.le@dicom.com>
2017-05-16 15:56:05 -0400

    test state

--- 0-test/src/js/components/Layout.js
+++ 0-test/src/js/components/Layout.js
@@ -3,11 +3,12 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
+import Title from "./Header/Title";
+
 export default class Layout extends React.Component {
   constructor() {
+
     super();
-
-
     this.state = {
       title: "Welcome wwww",
     };
@@ -32,18 +33,26 @@ export default class Layout extends React.Component {
       lastName: 'Perez'
     };
 
-    let element = (
-      <h1>
-        Hello, {formatName(user)}!
-      </h1>
+    let element = (      
+       <h1> Hello, {formatName(user)}! </h1>
     );
 
     const name = 'will';
 
+    var list = [ <Header title='test1'/>,<Header title='test2'/> ];
+
+    setTimeout(() => {
+     // this.setState({title: 'Vu Le'})
+      this.changeTitle('asdfasdf')
+    }, 3000)
+
     return (
-        <h1>
-           <br/>{element}
-        </h1>
+        <div>
+           {this.state.title} <br/>
+           <Header title='my test'/>
+           <Footer/>
+
+        </div>
     );
   }
 }
