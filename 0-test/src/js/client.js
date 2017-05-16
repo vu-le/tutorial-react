b324dec6b0da78c38323e28e1983ef78443ee525
vu-le <vu.le@dicom.com>
2017-05-16 15:19:23 -0400

    update test

--- 0-test/src/js/client.js
+++ 0-test/src/js/client.js
@@ -3,5 +3,39 @@ import ReactDOM from "react-dom";
 
 import Layout from "./components/Layout";
 
+/*
+class Test extends React.Component {
+    render() {
+
+        function formatName(user) {
+          return user.firstName + ' ' + user.lastName;
+        }
+
+        let user = {
+          firstName: 'Harper 333333',
+          lastName: 'Perez'
+        };
+
+
+        let element = (
+          <h1>
+            Hello, {formatName(user)}!
+          </h1>
+        );
+
+        const name = 'will';
+
+        return (
+            <h1>
+                <br/>{formatName(user)}
+            </h1>
+        )
+    }
+}
+*/
+
 const app = document.getElementById('app');
+
+//ReactDOM.render(element, app);
+//ReactDOM.render(<Test/>, app);
 ReactDOM.render(<Layout/>, app);
--- 0-test/src/js/components/Layout.js
+++ 0-test/src/js/components/Layout.js
@@ -6,6 +6,8 @@ import Header from "./Header";
 export default class Layout extends React.Component {
   constructor() {
     super();
+
+    
     this.state = {
       title: "Welcome wwww",
     };
@@ -15,12 +17,33 @@ export default class Layout extends React.Component {
     this.setState({title});
   }
 
+/*
+      <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+      <Footer />  
+*/
+
   render() {
+    function formatName(user) {
+      return user.firstName + ' ' + user.lastName;
+    }
+
+    let user = {
+      firstName: 'Harper ',
+      lastName: 'Perez'
+    };
+
+    let element = (
+      <h1>
+        Hello, {formatName(user)}!
+      </h1>
+    );
+
+    const name = 'will';
+
     return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
-      </div>
+        <h1>
+           <br/>{element}
+        </h1>
     );
   }
 }
